65ef58a327be536c74822986915532ca84591cdf
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2023-12-16 23:13:01 +0900

    에셋 추가

--- Assets/02.Scripts/Manager/GameManager.cs
+++ Assets/02.Scripts/Manager/GameManager.cs
@@ -6,13 +6,30 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    public bool mouseCursor = false;
+
     void Start()
     {
         instance = this;
+        MouseCursorSetting(mouseCursor);
     }
 
     void Update()
     {
-           
+
+    }
+
+    void MouseCursorSetting(bool flag)
+    {
+        if (flag == false)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
     }
 }
--- Assets/02.Scripts/Manager/InteractiveManger.cs
+++ Assets/02.Scripts/Manager/InteractiveManger.cs
@@ -4,33 +4,23 @@ using UnityEngine;
 
 public class InteractiveManger : MonoBehaviour
 {
-    
+    Ray ray;
+    RaycastHit hit;
+    float MaxDistance = 30f; 
+
     void Start()
     {
-        
+ 
     }
 
     void Update()
     {
-        
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, MaxDistance))
+        {
+            Debug.Log("Hit + " + hit.transform.name);
+        }
     }
 
-    // 트리거 영역에 객체가 들어올 때 호출됩니다.
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Trigger Entered by " + other.gameObject.name);
-    }
 
-    // 트리거 영역 안에서 객체가 지속될 때 매 프레임마다 호출됩니다.
-    private void OnTriggerStay(Collider other)
-    {
-        // 지속적인 트리거 처리
-        Debug.Log("Triggering " + other.gameObject.name); 
-    }
-
-    // 트리거 영역에서 객체가 나갈 때 호출됩니다.
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log("Trigger Exited by " + other.gameObject.name);
-    }
 }
